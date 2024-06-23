2d24d0043688c3a2e591cec1437bc3614dc2860a
BANK\dunglv2.os <dunglv2.os@mbbank.com.vn>
2024-06-23 21:31:34 +0700

    deploy prod

--- apps/hub-center/src/environments/environment.local.ts
+++ apps/hub-center/src/environments/environment.local.ts
@@ -1,8 +1,8 @@
 import { Environment } from '@hub-center/hub-model';
 
 class EnvironmentImpl implements Environment {
-  hubBackendApiEndpoint = 'https://cms.advtv.vn/api';
-  hubBackendApiEndpointFile = 'https://cms.advtv.vn/api';
+  hubBackendApiEndpoint = 'https://cms.advtv.vn/api/';
+  hubBackendApiEndpointFile = 'https://cms.advtv.vn/api/';
 }
 
 export const environment = new EnvironmentImpl();
